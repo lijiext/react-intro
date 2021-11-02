59d55e358da03037cdd53f0c035c9de76d74ffb0
Li Jie <lijiecn@vip.qq.com>
2021-11-02 10:07:21 +0000

    redux fix warning

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,61 +1,70 @@
 import React, {Component} from "react";
-import 'antd/dist/antd.css'
-import {Input, Button, List} from 'antd';
-import store from "./store";
+import {Input, Button, List} from "antd";
+import 'antd/dist/antd.css';
+import store from "./store/index.js";
 
 class ToDoList extends Component {
   constructor(props) {
     super(props);
-    this.state = store.getState();
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.state = store.getState();
+    console.log(this.state);
     this.handleStoreChange = this.handleStoreChange.bind(this);
     this.handleButtonClick = this.handleButtonClick.bind(this);
-
-    // 订阅 store 改变事件
+  };
+  // 需要在组件加载之后，给 store 注册监听器
+  // https://reactjs.org/blog/2015/12/16/ismounted-antipattern.html
+  componentDidMount() {
+    // store 发生变化，执行此函数
     store.subscribe(this.handleStoreChange);
-  }
-
-  render() {
-    return (
-      <div style={{margin: '10px'}}>
-        <div>
-          <Input
-            value={this.state.inputValue}
-            placeholder={'Input Here'}
-            style={{width: '300px', marginRight: '10px'}}
-            onChange={this.handleInputChange}
-          />
-          <Button type={'primary'} onClick={this.handleButtonClick}>提交</Button>
-        </div>
-        <List
-          style={{marginTop: '10px', width: '370px'}}
-          bordered
-          dataSource={this.state.list}
-          renderItem={(item, index) => (<List.Item>{item}</List.Item>)}
-        />
-      </div>
-    )
-  }
-
-  handleInputChange(e) {
-    const action = {
-      type: 'change input value',
-      value: e.target.value
+  };
+    render() {
+        return (
+          <div style={{padding: '10px'}}>
+            <Input 
+              placeholder='Please Input Here'
+              value={this.state.inputValue}
+              style={{width: '300px'}}
+              onChange={this.handleInputChange}
+              ></Input>
+              <Button 
+              type='primary' 
+              style={{marginLeft: '10px'}}
+              onClick={this.handleButtonClick}
+            >Submit</Button>
+            <List 
+              style={{width: '300px', marginTop: '10px'}} 
+              bordered
+              dataSource={this.state.list} 
+              renderItem={item => (<List.Item>{item}</List.Item>)}
+            />
+          </div>
+        )
     }
-    store.dispatch(action);
-  }
-
-  handleStoreChange() {
-    // 更新组件状态, 将数据同步
-    this.setState(store.getState());
-  }
-
-  handleButtonClick() {
-    const action = {
-      type: 'add todo item'
+    handleInputChange(e) {
+      const action = {
+        type: 'CHANGE_INPUT_VALUE',
+        value: e.target.value
+      };
+      store.dispatch(action);
+    }
+    handleStoreChange() {
+      console.log('Store Changed!!!');
+      // 获取 store 中的最新数据，并更新组件的状态
+      this.setState(store.getState());
     };
-    store.dispatch(action);
-  }
+    handleButtonClick() {
+      const action = {
+        type: 'ADD_TODO_ITEM'
+      };
+      store.dispatch(action);
+    }
 }
-
+/*
+1. 改变 store 数据
+2. 使用 action 告知 reducer
+3. store.dispatch(action)
+4. store 收到数据和 action 传递给 reducer
+*/
+    
 export default ToDoList;
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,23 +1,28 @@
 const defaultState = {
-  inputValue: '',
-  list: ['Task 1']
+    inputValue: '',
+    list: [
+        'Task 1',
+        'Task 2', 
+        'Task 3'
+    ]
+}
+// 某些 js 引擎不支持导出无名函数
+const reducer = (state = defaultState, action) => {
+    console.log(state, action);
+    if (action.type === 'CHANGE_INPUT_VALUE') {
+        // 深拷贝，不修改原来的数据，固定写法
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.inputValue = action.value;
+        return newState;
+    }
+    if (action.type === 'ADD_TODO_ITEM') {
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.list.push(newState.inputValue);
+        newState.inputValue = '';
+        return newState;
+    }
+    return state;
 };
 
-// reducer 的固定写法，可以接收 state，但是不能修改 state
-export default (state = defaultState, action) => {
-  if (action.type === 'change input value') {
-    // 对象深拷贝， 改变 state
-    const newState = JSON.parse(JSON.stringify(state));
-    newState.inputValue = action.value;
-    // return 给 store
-    return newState;
-  }
+export default reducer;
 
-  if (action.type === 'add todo item') {
-    const newState = JSON.parse(JSON.stringify(state));
-    newState.list.push(newState.inputValue);
-    newState.inputValue = '';
-    return newState;
-  }
-  return state;
-}
